23fd43c2d866d281244b2824f26f3e4a48021d58
Samuel Lord <samuel.lord@demarque.com>
2023-12-03 17:15:23 -0500

    day 1 part 2

---
+++ src/1.2.rb
@@ -0,0 +1,30 @@
+filename = '../inputs/1.txt'
+content = IO.read(filename)
+total = 0
+
+# hehehehe
+map = {
+  'zero' => 'ze0ro',
+  'one' => 'o1ne',
+  'two' => 'tw2o',
+  'three' => 'thr3ee',
+  'four' => 'fo4ur',
+  'five' => 'fi5ve',
+  'six' => 's6ix',
+  'seven' => 'se7ven',
+  'eight' => 'ei8ght',
+  'nine' => 'ni9ne'
+}
+
+content.each_line do |line|
+  map.each do |key, value|
+    line.gsub!(/#{key}/, value)
+  end
+
+  first_digit = line[/\d/]
+  last_digit = line[/\d(?=\D*$)/]
+  number = first_digit + last_digit
+  total += number.to_i
+end
+
+puts total
